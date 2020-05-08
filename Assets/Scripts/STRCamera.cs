fe107319339a55135a6d6f3e8b8f7b569b0ff45a
Noxalus <noxalus@gmail.com>
2020-05-08 15:40:23 +0200

    Create a grid shader in world space.

---
+++ Assets/Scripts/STRCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class STRCamera : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple Strategy Game
